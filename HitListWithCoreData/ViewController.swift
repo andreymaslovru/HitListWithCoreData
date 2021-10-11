8befd7de65c8eb12263460d176e8e41574919074
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-11 11:04:59 +0700

    added action of alert for click on "add"

--- HitListWithCoreData/ViewController.swift
+++ HitListWithCoreData/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+  
+    var names = [String]()
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func addAuction(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "New name", message: "Add a new name", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) {
+            (action: UIAlertAction!) -> Void in
+            let textField = alert.textFields?[0] as! UITextField
+            self.names.append(textField.text ?? "")
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) {
+            (action) -> Void in
+        }
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "List of names"
+        tableView.register(UITableView.self, forCellReuseIdentifier: "Cell")
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath as IndexPath)
+        cell.textLabel!.text = names[indexPath.row]
+        return cell
+    }
 }
 
