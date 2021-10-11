bea2e6fff1df9baa85c1fcd1e02116f24469e8ba
Andrey Maslov <andrey.ma2001@gmail.com>
2021-10-11 11:35:39 +0700

    fix bugs and added input on alert

--- HitListWithCoreData/ViewController.swift
+++ HitListWithCoreData/ViewController.swift
@@ -18,13 +18,16 @@ class ViewController: UIViewController, UITableViewDataSource {
         let alert = UIAlertController(title: "New name", message: "Add a new name", preferredStyle: .alert)
         let saveAction = UIAlertAction(title: "Save", style: .default) {
             (action: UIAlertAction!) -> Void in
-            let textField = alert.textFields?[0] as! UITextField
+            let textField = alert.textFields![0] as! UITextField
             self.names.append(textField.text ?? "")
             self.tableView.reloadData()
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .default) {
             (action) -> Void in
+        }
+        alert.addTextField {(textField: UITextField!) -> Void in
+            textField.placeholder = "Type..."
         }
         alert.addAction(saveAction)
         alert.addAction(cancelAction)
@@ -35,7 +38,7 @@ class ViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "List of names"
-        tableView.register(UITableView.self, forCellReuseIdentifier: "Cell")
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
